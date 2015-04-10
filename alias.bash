1986610ba2b108c5784c8e46744e520317a01690
Aaron Blasdel <ablasdel@users.noreply.github.com>
2015-04-10 08:40:46 -0700

    some useful git aliases

---
+++ alias.bash
@@ -0,0 +1,27 @@
+
+rosm () {
+    if [[ $@ ]]; then
+        export ROS_MASTER_URI="http://$@:11311"
+    else
+        export ROS_MASTER_URI="http://localhost:11311"
+    fi;
+    echo $ROS_MASTER_URI;
+}
+
+gitcommit () {
+    cmd="git commit -m \"$@\"";
+    eval $cmd;
+}
+
+
+gitaddcommit () {
+    cmd="git commit -a -m \"$@\"";
+    eval $cmd;
+}
+
+alias gitstatus='git status -s'
+alias gitdiff='git diff'
+alias gituncommit="git reset --soft 'HEAD^'"
+alias gitfileunaddrecursive="git reset HEAD "
+alias gitredo='git commit -c ORIG_HEAD'
+
